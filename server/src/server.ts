71b7a69cc03244b1d8cf952852d5428787c9b595
Luiz Matos <luizmatos88@yahoo.com.br>
2020-06-01 09:29:05 -0300

    Adicionado ts config Adicionado recarregamento automático ao atualizar arquivos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
 import express from "express"
+
+const app = express()
+
+app.get("/user", (req, res) => {
+  return res.json({ ok: true })
+})
+app.get("", (req, res) => {
+  return res.json({ inicio: true })
+})
+
+app.listen(3333)

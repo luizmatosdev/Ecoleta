b3c53f3359d50bf6c83e7e7969981638f9b932c3
Luiz Matos <luizmatos88@yahoo.com.br>
2020-06-03 21:26:48 -0300

    Adicionada página inicial e página de criar ponto de coleta

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import "./App.css"
 
+import Router from "./routes"
+
 function App() {
   return (
     <div className="App">
-      <h1>Olá</h1>
+      <Router />
     </div>
   )
 }

7d52ad3e66b10ca67148fa24e53b37a389e4b45e
Luiz Matos <luizmatos88@yahoo.com.br>
2020-06-04 21:21:52 -0300

    Finalizado Web

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, ChangeEvent } from "react"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState, ChangeEvent, FormEvent } from "react"
+import { Link, useHistory } from "react-router-dom"
 import { FiArrowLeft } from "react-icons/fi"
 import { Map, TileLayer, Marker } from "react-leaflet"
+import { LeafletMouseEvent } from "leaflet"
 
 import axios from "axios"
 import api from "../../services/api"
@@ -30,11 +31,26 @@ interface IBGECity {
 }
 
 const CreatePoint = () => {
+  const history = useHistory()
   const [items, setItems] = useState<Item[]>([])
   const [ufs, setUfs] = useState<UF[]>([])
   const [selectedUF, setSelectedUF] = useState("0")
   const [cities, setCities] = useState<string[]>([])
   const [selectedCity, setSelectedCity] = useState("0")
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0,
+    0,
+  ])
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ])
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    whatsapp: "",
+  })
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
 
   useEffect(() => {
     api
@@ -84,12 +100,67 @@ const CreatePoint = () => {
       })
   }, [selectedUF])
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords
+      setInitialPosition([latitude, longitude])
+    })
+  }, [])
+
   function hundleSelectUF(event: ChangeEvent<HTMLSelectElement>) {
     setSelectedUF(event.target.value)
   }
 
   function hundleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
     setSelectedCity(event.target.value)
+  }
+
+  function hundleMapClick(event: LeafletMouseEvent) {
+    const { lat, lng } = event.latlng
+    setSelectedPosition([lat, lng])
+  }
+
+  function hundleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { value, name } = event.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  function hundleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex((item) => item === id)
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id)
+      setSelectedItems(filteredItems)
+    } else {
+      setSelectedItems([...selectedItems, id])
+    }
+  }
+
+  function hundleSubmit(event: FormEvent) {
+    event.preventDefault()
+    const { name, email, whatsapp } = formData
+    const uf = selectedUF
+    const city = selectedCity
+    const [latitude, longitude] = selectedPosition
+    const items = selectedItems
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items,
+    }
+    api
+      .post("/points", data)
+      .then(() => {
+        alert("Ponto criado")
+        history.push("/")
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   return (
@@ -101,7 +172,7 @@ const CreatePoint = () => {
           Voltar para home
         </Link>
       </header>
-      <form>
+      <form onSubmit={hundleSubmit}>
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
@@ -111,16 +182,31 @@ const CreatePoint = () => {
           </legend>
           <div className="field">
             <label htmlFor="name">Nome da entidade</label>
-            <input type="text" name="name" id="name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={hundleInputChange}
+            />
           </div>
           <div className="field-group">
             <div className="field">
               <label htmlFor="email">E-mail</label>
-              <input type="email" name="email" id="email" />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={hundleInputChange}
+              />
             </div>
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
-              <input type="text" name="whatsapp" id="whatsapp" />
+              <input
+                type="text"
+                name="whatsapp"
+                id="whatsapp"
+                onChange={hundleInputChange}
+              />
             </div>
           </div>
         </fieldset>
@@ -129,12 +215,12 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
-          <Map center={[-15.8289662, -48.0506021]} zoom={15}>
+          <Map center={initialPosition} zoom={15} onClick={hundleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[-15.8289662, -48.0506021]} />
+            <Marker position={selectedPosition} />
           </Map>
           <div className="field-group">
             <div className="field">
@@ -178,7 +264,11 @@ const CreatePoint = () => {
           </legend>
           <ul className="items-grid">
             {items.map((item) => (
-              <li key={item.id}>
+              <li
+                key={item.id}
+                onClick={() => hundleSelectItem(item.id)}
+                className={selectedItems.includes(item.id) ? "selected" : ""}
+              >
                 <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
